df90718cc171e1cee8ac42aeea47718584e56203
Hantsaniala Eléo <hantsaniala@gmail.com>
2019-03-20 11:59:29 +0300

    Adding some missing patterns

--- regex.js
+++ regex.js
@@ -18,24 +18,25 @@
 
  /**
   * [syntaxe] =>  [fanazavana]
-  * .         => izay caractere rehetra ohatra /.a/ -> "ma"(marina), "androany"(diso satria tsisy soratra eo alohany)
-  * \w        => mitovy amin'ny hoe [a-zA-Z0-9_]
-  * \d        => mitovy amin'ny hoe [0-9]
-  * \n        => midina andalana
+  * .         => Izay caractère rehetra ohatra /.a/ -> "ma"(marina), "androany"(diso satria tsisy soratra eo alohany)
+  * \w        => Mitovy amin'ny hoe [a-zA-Z0-9_]
+  * \W        => Mandray izay caratère rehetra tsy alphanumérique
+  * \d        => Mitovy amin'ny hoe [0-9]
+  * \D        => Mandray izay caractère rehetra tsy numérique, izany hoe izay [0-9] rehetra
+  * \n        => Midina andalana
   * \t        => Tabulation
   * \.        => Consideration an'ny . test\.com
-  * \s        => Maka ny espace 
-  * \S        => Maka iza mots tsisy espace
-  * \W        => Maka izay tsy mots alphanumérique
+  * \s        => Mandray ny karazana espace rehetra, tafiditra ao anatiny ny \t 
+  * \S        => Mandray izay caractère tsy espace rehetra
   */
 
   /**
    * [syntaxe]          => [fanazavana]                             -> [ohatra]
-   * [a-z][0-9][A-Z]    => izany hoe misy mots telo farafahakeliny  -> madama(marina) , ma(diso) 
-   * {min,max}          => eo anelanelan'ny min sy max              -> {5,9} , mamama(marina),mama(diso)
-   * {min,}             => valeur min à l'infini                    -> {6,} , mamamammamamamammama(marina), mamam(diso)
-   * {,max}             => 0 à max                                  -> {,6} , mamamammamamamammama(diso), mamam(marina)
-   * {nombre}           => isany farafahakely                       -> {6}  , mama(diso), aamamamam(marina)
+   * [a-z][0-9][A-Z]    => Izany hoe misy mots telo farafahakeliny  -> madama(marina), ma(diso) 
+   * {min,max}          => Eo anelanelan'ny min sy max              -> {5,9}, mamama(marina), mama(diso)
+   * {min,}             => Valeur min à l'infini                    -> {6,}, mamamammamamamammama(marina), mamam(diso)
+   * {,max}             => 0 à max                                  -> {,6}, mamamammamamamammama(diso), mamam(marina)
+   * {nombre}           => Isany farafahakely                       -> {6}, mama(diso), aamamamam(marina)
    */
 
 /**
@@ -53,5 +54,5 @@ console.log(matchUrl.test("www.google.com")); //true
 console.log(matchUrl.test("www.yahoo.za"));   //false
 console.log(matchUrl3.test(" www.yahoo.fr")); //true satria misy espace eo alohany
 console.log(matchUrl3.test(" www.yahoo.fr")); //false satria misy espace eo alohany
-console.log(match4.test("*/-+\.#'~"));        // true satria tsy alphanumérique ireo mots ireo
+console.log(match4.test("*/-+\.#'~"));        //true satria tsy alphanumérique ireo mots ireo
 
