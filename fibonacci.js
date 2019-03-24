2f932a289fb1e81b95f0ed7f5779adcc31d132be
Julien RAJERISON <julienrajerison5@gmail.com>
2019-03-24 19:07:27 +0300

    Update and rename fibonnaci.js to fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,13 @@
 /*
-* RAJERISON Julien
-* BIG 0 notation && Algorithm fibonacci
-* TABLEAU FIBONACCI
-* n  0 1 2 3 4 5 6 7
-* xn 0 1 1 2 3 5 8 13
-* xn = |(xn-1) + (xn-2)|
+* @Author RAJERISON Julien
+* @Description BIG-O notation && Algorithm fibonacci
+*----------------------------------- 
+*         TABLEAU FIBONACCI
+*-----------------------------------
+* n  0 | 1 | 2 | 3 | 4 | 5 | 6 | 7  |
+* xn 0 | 1 | 1 | 2 | 3 | 5 | 8 | 13 |
+*----------------------------------- 
+*xn = |(xn-1) + (xn-2)|
 */
 
 // Is constant <Time complexity Constant | Space complexity = 0(1) Constant>
